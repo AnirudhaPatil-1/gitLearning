41231f1272ede03b61e45d836fd7805edeec051b
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-02-01 16:08:15 +0530

    file changes

--- steps.js
+++ steps.js
@@ -1,6 +1,9 @@
+//git --version
 //git init
 //git remote add origin "the github repository http address"
 //git add . => track files
 //git status
 //git branch
+// git pus]]h origin  master
+//git log
 
